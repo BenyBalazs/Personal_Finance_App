494057e02125e9e859294a35d13a2e82b9dd1ca0
Benyovszki_Balazs <gnuki2000@gmail.com>
2020-04-13 13:05:56 +0200

    Created the basic layout of the EditWindow

--- src/main/java/org/openjfx/EditWindowController.java
+++ src/main/java/org/openjfx/EditWindowController.java
@@ -1,4 +1,17 @@
 package org.openjfx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.nio.Buffer;
+
 public class EditWindowController {
+
+    @FXML
+    Button listUpdaterButton;
+
+    @FXML
+    void updateList(){
+
+    }
 }
